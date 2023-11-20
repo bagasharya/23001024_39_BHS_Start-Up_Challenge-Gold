6bbf68919a46f65663d947f9ddd449fa847f63ed
bagas harya <bagasharya@s.local>
2023-11-20 20:05:49 +0700

    add static file

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require('express');
-const landingController = require('./controllers/landing');
-
 const app = express();
-
 app.set('view engine', 'ejs');
+app.use('/assets',express.static('assets'))
 
-app.get('/', landingController.landing);
+app.get('/', (req, res) => {
+  const produk = [
+    {
+      brand: 'bolt',
+      namaProduk: 'bolt ikan',
+      harga: '18500',
+    },
+  ];
+
+  res.render('landing-page', {
+    produk,
+  });
+});
 
 app.listen(1997, () => {
   console.log('Server Menyalaa');
