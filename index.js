f668a218f7992b9459486897a2b3dee6064a8e77
bagas harya <bagasharya@s.local>
2023-11-23 10:35:44 +0700

    change dashboard view

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 app.set('view engine', 'ejs');
-app.use('/assets',express.static('assets'))
+app.use(express.static('views'));
+app.use('/assets',express.static('assets'));
 
 app.get('/', (req, res) => {
   const produk = [
@@ -15,6 +16,11 @@ app.get('/', (req, res) => {
   res.render('landing-page', {
     produk,
   });
+});
+
+
+app.get('/dashboard', (req, res) => {
+  res.render('dashboard');
 });
 
 app.listen(1997, () => {
